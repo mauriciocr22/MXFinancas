0564658a938b0c0fd62ab407125cfcd86c703121
Maurício de Carvalho <mauriciocr223@gmail.com>
2024-09-28 21:15:26 -0300

    feat: changed express to fastify

--- app.ts
+++ app.ts
@@ -1,9 +1,20 @@
-import express from "express";
+import fastify from "fastify";
+import cors from "@fastify/cors";
 
-const app = express();
+const app = fastify();
 
-app.use(express.json());
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000 ⭐");
+app.register(cors, {
+  origin: true,
 });
+
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 });
+    console.log("⭐ Server running on port 3000 ⭐");
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
